a86c9caa21c2810b93f190e46518d4d8e545d0ef
Sheker <hugoboyat07@gmail.com>
2023-08-21 22:08:53 +0200

    Added Basic UI for Business

--- Systems/Database.Business.cs
+++ Systems/Database.Business.cs
@@ -40,6 +40,7 @@ namespace Roleplay.Systems
 
 			if (ownerPerson != null)
             {
+                newBusiness.SetPersonOwner(ownerPerson);
 				Event.Run(Events.Business.Server.PersonCreatedBusinessID, ownerPerson, business);
                 ResponseOnSelfJoin(business.Serialize());
                 return newBusiness;
